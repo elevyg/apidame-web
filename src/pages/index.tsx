76817e8d79e2584da18575022c9f12cd74d531bf
elevyg <elevyg91@gmail.com>
2022-12-06 11:33:38 -0300

    feat: Og Image added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import Logo from "assets/svgs/logo.svg";
 import { type NextPage } from "next";
-import { CldImage } from "next-cloudinary";
+import { CldImage, CldOgImage } from "next-cloudinary";
 
 const Home: NextPage = () => {
   return (
     <>
-      <main className="flex min-h-screen flex-col ">
+      <CldOgImage
+        src="apidame-web/splash-image"
+        alt="Climber dynoing"
+        width={2400}
+        height={1200}
+        overlays={[
+          {
+            publicId: "apidame-web/logo-white",
+            width: 800,
+            height: 800,
+          },
+        ]}
+      />
+      <main className="flex flex-col">
         <div className="flex">
           <div className="z-10 m-6 hover:animate-spin">
             <Logo height={200} width={200} className="fill-primaryB" />

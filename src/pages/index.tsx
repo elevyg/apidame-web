0b9e45d6283596e0c4b735850cf4dc6e46c5b192
elevyg <elevyg91@gmail.com>
2023-12-30 09:52:21 -0300

    feat: new landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,32 @@
-import Logo from "assets/svgs/logo.svg";
 import { type NextPage } from "next";
-import { CldImage, CldOgImage } from "next-cloudinary";
+import { CldImage } from "next-cloudinary";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <CldOgImage
-        src="apidame-web/splash-image"
-        alt="Climber dynoing"
-        width={2400}
-        height={1200}
-        overlays={[
-          {
-            publicId: "apidame-web/logo-white",
-            width: 800,
-            height: 800,
-          },
-        ]}
-      />
-      <main className="flex flex-col">
-        <div className="flex">
-          <div className="z-10 m-6 hover:animate-spin">
-            <Logo height={200} width={200} className="fill-primaryB" />
-          </div>
+    <main className="flex flex-col">
+      <div className="h-screen">
+        <div className="absolute inset-0 z-10 flex items-center justify-center  overflow-hidden">
           <CldImage
-            src="apidame-web/splash-image"
-            alt="Climber dynoing"
-            width={4752}
-            height={3168}
-            className="absolute inset-0 h-full w-full object-cover"
-            sizes="100vw"
+            src="apidame-web/vamos-a-escalar-complete"
+            alt="Lets go climbing"
+            width={2000}
+            height={2000}
+            className="w-2/3"
             placeholder="blur"
           />
         </div>
-      </main>
-    </>
+        <CldImage
+          src="apidame-web/noise-background"
+          alt="Climber dynoing"
+          width={4752}
+          height={3168}
+          className="absolute inset-0 h-full w-full object-cover"
+          sizes="100vw"
+          placeholder="blur"
+        />
+      </div>
+      <div className="h-screen bg-primaryB"></div>
+    </main>
   );
 };
 

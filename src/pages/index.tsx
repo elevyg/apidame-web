fe94a21efe020ea9d5b505596451cac37861a358
elevyg <elevyg91@gmail.com>
2024-01-01 14:42:42 -0300

    feat: price and schedules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,16 +55,60 @@ const Home: NextPage = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2744.2544530120053!2d-71.71614262295716!3d-46.54265308138484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbded2b2de962d3c1%3A0x5b288c19fb231bd7!2sApidame%20Boulder!5e0!3m2!1ses!2scl!4v1704127632243!5m2!1ses!2scl"
             width="600"
             height="450"
-            className="min-w-[100%] flex-1 border-none md:h-auto md:min-w-[50%]"
+            className="min-w-[100%] flex-1 border-none md:h-full md:min-w-[50%]"
             allowFullScreen={true}
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
       </div>
-      <div className="flex h-screen justify-center bg-primaryB p-6">
-        <h2 className="font-forgen text-5xl">Precios y horarios</h2>
+      <div className="flex flex-col justify-evenly gap-4 bg-primaryB p-6 md:flex-row">
+        <div>
+          <h2 className="font-forgen text-5xl">Horarios</h2>
+          <div className="flex flex-col gap-4">
+            <div>
+              <h3 className="text-xl">Martes a Viernes</h3>
+              <p>17:00 a 22:00</p>
+            </div>
+            <div>
+              <h3 className="text-xl">Sábado</h3>
+              <p>10:00 a 14:00</p>
+              <p>17:00 a 22:00</p>
+            </div>
+            <div>
+              <h3 className="text-xl">Domingo y festivos</h3>
+              <p>Cerrado en principio</p>
+              <p>
+                Cualquier cambio se publicará en nuestro Instagram
+                @apidameboulder
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className="font-forgen text-5xl">Precios</h2>
+          <div className="flex flex-col gap-4">
+            <div>
+              <h3 className="text-xl">Diario</h3>
+              <p>$6.000</p>
+            </div>
+            <div>
+              <h3 className="text-xl">3 días</h3>
+              <p>$15.000</p>
+            </div>
+            <div>
+              <h3 className="text-xl">1 semana</h3>
+              <p>$20.000</p>
+            </div>
+          </div>
+        </div>
       </div>
+      <footer className="flex items-center justify-center gap-4 bg-primaryA py-4 md:py-6">
+        <Logo height={50} width={50} className="fill-primaryB" />
+        <h1 className="font-holluise text-2xl tracking-[0.4em] text-primaryB md:text-5xl">
+          APIDAME BOULDER
+        </h1>
+      </footer>
     </main>
   );
 };

c83be374b133a9bff3ad79249bf479a099db5b4a
elevyg <elevyg91@gmail.com>
2024-01-01 14:32:34 -0300

    feat: location

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,24 @@
 import { type NextPage } from "next";
-import Logo from "assets/svgs/logo.svg";
+import Logo from "assets/svgs/icon-solo.svg";
 import { CldImage } from "next-cloudinary";
 
 const Home: NextPage = () => {
   return (
     <main className="flex flex-col">
-      <nav className="flex h-24 items-center justify-center gap-2 bg-primaryA">
+      <nav className="flex items-center justify-center gap-4 bg-primaryA py-4 md:py-6">
         <Logo height={50} width={50} className="fill-primaryB" />
-        <h1 className="font-holluise text-5xl tracking-[0.4em] text-primaryB">
+        <h1 className="font-holluise text-2xl tracking-[0.4em] text-primaryB md:text-5xl">
           APIDAME BOULDER
         </h1>
       </nav>
-      <div className="relative h-screen">
-        <div className="absolute inset-0 z-10 flex items-center justify-center  overflow-hidden">
+      <div className="relative h-[60vh] md:h-screen">
+        <div className="absolute inset-0 z-10 flex items-center justify-center overflow-hidden">
           <CldImage
             src="apidame-web/vamos-a-escalar-complete"
             alt="Lets go climbing"
             width={2000}
             height={2000}
-            className="w-2/3"
+            className="px-10 md:w-2/3"
             placeholder="blur"
           />
         </div>
@@ -31,6 +31,36 @@ const Home: NextPage = () => {
           sizes="100vw"
           placeholder="blur"
         />
+      </div>
+      <div className="flex flex-col justify-between bg-primaryA md:flex-row ">
+        <div className="flex flex-1 flex-col items-center justify-center gap-3 p-3 py-6">
+          <h2 className="font-forgen text-5xl text-primaryB">Ubicación</h2>
+          <p className="font-brown text-primaryB">
+            Nuestro centro se ubica en por el Camino Internacional, a 200 metros
+            del límite urbano de Chile Chico.
+          </p>
+          <CldImage
+            src="apidame-web/apidame_street_sign_pnneko"
+            alt="Entrance to Apidame Boulder"
+            width={300}
+            height={300}
+            className="object-cover"
+            sizes="100vw"
+            placeholder="blur"
+            aspectRatio="1:1"
+          />
+        </div>
+        <div className="h-96 md:h-auto">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2744.2544530120053!2d-71.71614262295716!3d-46.54265308138484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbded2b2de962d3c1%3A0x5b288c19fb231bd7!2sApidame%20Boulder!5e0!3m2!1ses!2scl!4v1704127632243!5m2!1ses!2scl"
+            width="600"
+            height="450"
+            className="min-w-[100%] flex-1 border-none md:h-auto md:min-w-[50%]"
+            allowFullScreen={true}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </div>
       <div className="flex h-screen justify-center bg-primaryB p-6">
         <h2 className="font-forgen text-5xl">Precios y horarios</h2>

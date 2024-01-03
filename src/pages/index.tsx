7c6d60f09d903e1d6ea865834213489f93838ca3
elevyg <elevyg91@gmail.com>
2024-01-03 10:12:09 -0300

    fix: prices

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,8 +32,8 @@ const Home: NextPage = () => {
           placeholder="blur"
         />
       </div>
-      <div className="flex flex-col justify-between bg-primaryA md:flex-row ">
-        <div className="flex flex-1 flex-col items-center justify-center gap-3 p-10 ">
+      <div className="flex w-full flex-col justify-between bg-primaryA md:flex-row">
+        <div className="flex flex-col items-center justify-center gap-3  p-10 md:max-w-[50%]">
           <h2 className="font-forgen text-5xl text-primaryB">Ubicación</h2>
           <p className="font-brown text-primaryB">
             Nuestro centro se ubica en por el Camino Internacional, a 200 metros
@@ -51,20 +51,20 @@ const Home: NextPage = () => {
             aspectRatio="1:1"
           />
         </div>
-        <div className="h-96 md:h-auto">
+        <div className="h-96 w-full md:h-auto">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2744.2544530120053!2d-71.71614262295716!3d-46.54265308138484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbded2b2de962d3c1%3A0x5b288c19fb231bd7!2sApidame%20Boulder!5e0!3m2!1ses!2scl!4v1704127632243!5m2!1ses!2scl"
             width="600"
             height="450"
-            className="max-w-full flex-1 border-none md:h-full md:min-w-[50%]"
+            className="w-full min-w-full flex-1 border-none md:h-full"
             allowFullScreen={true}
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
       </div>
-      <div className="flex flex-col justify-evenly gap-4 bg-primaryB p-6 md:flex-row">
-        <div>
+      <div className="flex flex-col gap-4 bg-primaryB  md:flex-row">
+        <div className="p-6">
           <h2 className="font-forgen text-5xl">Horarios</h2>
           <div className="flex flex-col gap-4 font-brown">
             <div>
@@ -91,13 +91,22 @@ const Home: NextPage = () => {
             </div>
           </div>
         </div>
-        <Logo height={50} width={50} className="fill-primaryA md:hidden" />
-        <div>
+        <div className="flex w-full items-center justify-center md:hidden">
+          <Logo height={50} width={50} className="fill-primaryA" />
+        </div>
+        <div className="p-6">
           <h2 className="font-forgen text-5xl">Precios</h2>
           <div className="flex flex-col gap-4 font-brown">
             <div>
               <h3 className="text-xl">Diario</h3>
               <p>$6.000</p>
+            </div>
+            <div>
+              <h3 className="text-xl">Estudiantes y menores de 18 años*</h3>
+              <p>$4.000</p>
+              <p className="text-sm text-gray-700">
+                * Acreditar con carnet o alguna credencial de estudiante
+              </p>
             </div>
             <div>
               <h3 className="text-xl">3 días</h3>

79911b74f7473d34b9f2030c7fa45cd935443854
elevyg <elevyg91@gmail.com>
2024-01-06 09:58:01 -0300

    fix: link instead of a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,25 @@
 import { type NextPage } from "next";
 import Logo from "assets/svgs/icon-solo.svg";
 import { CldImage } from "next-cloudinary";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <main className="flex flex-col">
-      <nav className="flex items-center justify-center gap-4 bg-primaryA py-4 md:py-6">
-        <Logo
-          height={50}
-          width={50}
-          className="h-p[30px] w-[30px] fill-primaryB md:h-[50px] md:w-[50px]"
-        />
-        <h1 className="font-holluise text-xl tracking-[0.4em] text-primaryB md:text-5xl">
-          APIDAME BOULDER
-        </h1>
+      <nav className="">
+        <Link
+          className="flex items-center justify-center gap-4 bg-primaryA py-4 md:py-6"
+          href="/"
+        >
+          <Logo
+            height={50}
+            width={50}
+            className="h-p[30px] w-[30px] fill-primaryB md:h-[50px] md:w-[50px]"
+          />
+          <h1 className="font-holluise text-xl tracking-[0.4em] text-primaryB md:text-5xl">
+            APIDAME BOULDER
+          </h1>
+        </Link>
       </nav>
       <div className="relative h-[60vh] md:h-screen">
         <div className="absolute inset-0 z-10 flex items-center justify-center overflow-hidden">
@@ -68,12 +74,12 @@ const Home: NextPage = () => {
             >
               Inscríbete
             </a>
-            <a
+            <Link
               className=" rounded-md bg-blue-600 py-2 px-6 font-forgen tracking-wider text-white"
               href="/taller-escalada-kids"
             >
               Más información
-            </a>
+            </Link>
           </div>
         </div>
       </div>
--- src/pages/taller-escalada-kids.tsx
+++ src/pages/taller-escalada-kids.tsx
@@ -1,19 +1,25 @@
 import { type NextPage } from "next";
 import Logo from "assets/svgs/icon-solo.svg";
 import { CldImage } from "next-cloudinary";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <main className="flex flex-col">
-      <nav className="flex items-center justify-center gap-4 bg-primaryA py-4 md:py-6">
-        <Logo
-          height={50}
-          width={50}
-          className="h-p[30px] w-[30px] fill-primaryB md:h-[50px] md:w-[50px]"
-        />
-        <h1 className="font-holluise text-xl tracking-[0.4em] text-primaryB md:text-5xl">
-          APIDAME BOULDER
-        </h1>
+      <nav className="">
+        <Link
+          className="flex items-center justify-center gap-4 bg-primaryA py-4 md:py-6"
+          href="/"
+        >
+          <Logo
+            height={50}
+            width={50}
+            className="h-p[30px] w-[30px] fill-primaryB md:h-[50px] md:w-[50px]"
+          />
+          <h1 className="font-holluise text-xl tracking-[0.4em] text-primaryB md:text-5xl">
+            APIDAME BOULDER
+          </h1>
+        </Link>
       </nav>
       <div className="relative flex flex-col overflow-hidden p-10 md:flex-row">
         <div className="absolute inset-0  z-0 bg-[url(https://res.cloudinary.com/dzyy8nvgd/image/upload/c_scale,q_auto,w_500/v1704542059/apidame-web/vamos_a_escalar_kids_bg_t2l80k.png)] bg-repeat"></div>

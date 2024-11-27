34723515f6affea39b859365128734154e68dd2a
elevyg <elevyg91@gmail.com>
2024-11-27 15:45:22 -0300

    feat: topos

---
+++ src/pages/topos.tsx
@@ -0,0 +1,31 @@
+import { type NextPage } from "next";
+import Link from "next/link";
+import Logo from "assets/svgs/icon-solo.svg";
+
+const Topos: NextPage = () => {
+  return (
+    <main>
+      <nav className="">
+        <Link
+          className="flex items-center justify-center gap-4 bg-primaryA py-4 md:py-6"
+          href="/"
+        >
+          <Logo
+            height={50}
+            width={50}
+            className="h-p[30px] w-[30px] fill-primaryB md:h-[50px] md:w-[50px]"
+          />
+          <h1 className="font-holluise text-xl tracking-[0.4em] text-primaryB md:text-5xl">
+            APIDAME BOULDER
+          </h1>
+        </Link>
+      </nav>
+      <iframe
+        src="https://drive.google.com/embeddedfolderview?id=1-Be9uxgPKeD30336yjwvZa8czW9iBwNu#grid"
+        className="h-96 w-full border-none"
+      ></iframe>
+    </main>
+  );
+};
+
+export default Topos;
